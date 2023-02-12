54304ed69aba9290d66d7165c89540fe1bf1fbc4
raxhavijaya <raxhavijaya31@gmail.com>
2023-02-12 16:42:26 +0700

    portofolio

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -48,12 +48,10 @@ const SocialLinks = () => {
 
   return (
     <div className="hidden lg:flex flex-col top-[35%] left-0 fixed">
-        
       <ul>
         {links.map(({ id, child, href, style, download }) => (
           // eslint-disable-next-line no-useless-concat
           <li key={id} className={"flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500" + " " + style}>
-            
             <a href={href} className="flex justify-between w-full text-white" download={download} target="_blank" rel="noreferrer">
               {child}
             </a>
