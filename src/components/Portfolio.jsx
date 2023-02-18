f13836c917607f43eaa05d089f9e16558e155234
raxhavijaya <raxhavijaya31@gmail.com>
2023-02-19 01:50:30 +0700

    update portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,47 +1,53 @@
-import React from 'react'
-import BinarCarRent from '../assets/portfolio/BinarCarRent.jpg'
-const Portfolio = () => {
+import React from "react";
+import BinarCarRent from "../assets/portfolio/BinarCarRent.jpg";
+import QuotesGenerator from "../assets/portfolio/quotesgenerator.jpg"
 
-const portfolios = [
-  {
-    id:1,
-    src: BinarCarRent,
-  }
-]
+const Portfolio = () => {
+  const portfolios = [
+    {
+      id: 1,
+      src: BinarCarRent,
+      demo: "https://binar-car-rental-customer.netlify.app",
+      code: "https://github.com/ariqkharisma/challenge-binar-academy"
+    },
+    {
+      id: 2,
+      src: QuotesGenerator,
+      demo: "https://raxhaquotesgenerator31.netlify.app",
+      code: "https://github.com/raxhavijaya/quotegenerator"
+    },
+  ];
 
   return (
-    <div name='portfolio' className='bg-gradient-to-b from-black to-gray-800 w-full text-white pt-20 md:h-screen'>
-      <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
-        <div className='pb-8'>
-          <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Portfolio</p>
-          <p className='py-6'>Check out some of my work here</p>
+    <div name="portfolio" className="bg-gradient-to-b from-black to-gray-800 w-full text-white pt-20 md:h-screen">
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">Portfolio</p>
+          <p className="py-6">Check out some of my work here</p>
         </div>
 
-
-
-        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
-
-        {
-  portfolios.map(({id, src}) => (
-    <div key={id} className='shadow-md shadow-gray-600 rounded-lg'>
-    <img src={src} alt="" className='rounded-md duration-200 hover:scale-105' />
-    <div className='flex items-center justify-center'>
-      <button  className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-110' >
-        <a href='https://binar-car-rental-customer.netlify.app' target="_blank" rel="noreferrer">Demo</a>
-        </button>
-      <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-110' >
-      <a href='https://github.com/ariqkharisma/challenge-binar-academy' target="_blank" rel="noreferrer">Code</a>
-      </button>
-    </div>
-  </div>
-  ))
-}
-
-        
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+          {portfolios.map(({ id, src, demo, code }) => (
+            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+              <img src={src} alt="portfolioImage" className="imgPortfolio rounded-md duration-200 hover:scale-105" />
+              <div className="flex items-center justify-center">
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-110">
+                  <a href={demo} target="_blank" rel="noreferrer">
+                    Demo
+                  </a>
+                </button>
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-110">
+                  <a href={code} target="_blank" rel="noreferrer">
+                    Code
+                  </a>
+                </button>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
